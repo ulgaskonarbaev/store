aa74d37041a11c8c8f752f07c8ea723fddf7e38c
Ulgas <ulgas.konarbaev@bk.ru>
2023-04-29 21:18:16 +0600

    Add serch by Isbn and Author

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -8,7 +8,7 @@ namespace Store
 {
     public interface IBookRepository
     {
-        
-        Book[] GetAllByTitle(string titlePart);
+        Book[] GetAllByIsbn(string isbn);
+        Book[] GetAllByTitleOrAuthor(string titleOrAuthor);
     }
 }
