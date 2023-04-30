e1e5e56cefcd6ebd8f91bfe5efc04655205ec212
Ulgas <ulgas.konarbaev@bk.ru>
2023-04-30 15:50:52 +0600

    Add views and description for book

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -8,12 +8,16 @@ namespace Store
         public string Isbn { get; set; }
         public string Author { get; set; }
         public string Title { get; set; }
-        public Book(int id,string isbn,string author, string title)
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Book(int id,string isbn,string author, string title,string description,decimal price)
         { 
             Id = id;
             Isbn = isbn;
             Author = author;
             Title = title;
+            Description = description;
+            Price = price;
         }
 
         internal static bool IsIsbn(string s) 
