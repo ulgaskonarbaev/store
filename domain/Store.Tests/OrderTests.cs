89d69ebcf632d422c1870e4ff7c7dbdccddda002
Ulgas <ulgas.konarbaev@bk.ru>
2023-06-04 14:33:55 +0600

    Add order in order sclad

---
+++ domain/Store.Tests/OrderTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace Store.Tests
+{
+    public class OrderTests
+    {
+        public void Order_WithNullItems_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Order(1, null));
+        }
+
+        public void TotalCount_WithEmptyItems_ReturnsZero()
+        {
+            var order = new Order(1 , new OrderItem[0]);
+            Assert.Equal(0, order.TotalCount);
+        }
+    }
+}
--- presentation/Store.Web/Models/Cart.cs
+++ presentation/Store.Web/Models/Cart.cs
@@ -3,7 +3,16 @@ namespace Store.Web.Models
 {
     public class Cart
     {
-        public IDictionary<int,int> Items { get; set; } = new Dictionary<int,int>();
-        public decimal Amount { get; set; }
+      public int OrderId { get;}  
+        
+      public int TotalCount { get; set; }
+      public decimal TotalPrice { get; set; }
+
+        public Cart(int orderId)
+        {
+            OrderId = orderId;
+            TotalCount = 0;
+            TotalPrice = 0m;
+        }
     }
 }
